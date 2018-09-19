afcbb5f804d716314e5268f6badbafe48aed4b78
Ashwin van Dijk <contact@ashwinvandijk.nl>
2018-09-19 15:04:49 +0200

    Update

--- appdaemon/apps/onyourwayhome.py
+++ appdaemon/apps/onyourwayhome.py
@@ -1,32 +1,46 @@
 import appdaemon.appapi as appapi
 
 #
-# Hello World App
+# OnYourWayHome
 #
-# Args:
+# On every change from a sensor, checks if the value is lower then the previous change. If true, it adds one to the
+# counter. When it reaches the defined number it calls a script.
 #
 
 class OnYourWayHome(appapi.AppDaemon):
 
     def initialize(self):
+        self.counter = 0
+
         (self.distance, self.unit) = self.get_state(self.args["entity_id"], attribute="distance").split(" ")
-        self.log(self.distance)
         if self.unit == 'km':
             self.distance = float(self.distance) * 1000
-
-        self.log(self.distance)
-
-        self.counter = 0
 
         self.listen_state(self.checkValue, self.args["entity_id"])
 
     def checkValue(self, entity, attribute, old, new, kwargs):
-        if (float(new) >= float(old) + float(self.args["treshold"]) and float(new) > 10):
-            self.log(new)
+        self.log('---')
+        self.log(entity)
+        self.log(old)
+        self.log(new)
+
+        (self.newDistance, self.newUnit) = self.get_state(self.args["entity_id"], attribute="distance").split(" ")
+        if self.newUnit == 'km':
+            self.newDistance = float(self.distance) * 1000
+
+            self.log(self.newDistance)
+            self.log(self.distance)
+
+        if (float(self.distance) >= (float(self.newDistance) + float(self.args["treshold"])) and float(self.newDistance) > 1000):
+
+            self.distance = self.newDistance
             self.counter = self.counter + 1
 
-            if self.counter == self.args["moves"]:
+            self.log(self.counter)
+
+            if float(self.counter) == float(self.args["moves"]):
                 self.counter = 0
                 self.log("Script called")
-                self.turn_on(self.args["turn_on"], **kwargs)
+                self.turn_on(self.args["turn_on"])
 
+        self.log('---')
